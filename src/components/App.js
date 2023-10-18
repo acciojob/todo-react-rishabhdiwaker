7d4ad61ed328a204d5f1d6af89ccb265b6fdc430
rishabhdiwaker <rishabhdiwaker.rd@gmail.com>
2023-10-19 01:37:42 +0530

    submitting code-Thu Oct 19 2023 01:37:32 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -4,32 +4,34 @@ import './../styles/App.css';
 const App = () => {
 
   const [todo, setTodo] = useState("");
-  const [todoList, setTodoList] = useState([ ]);
-   
-  console.log("todo" ,todo)
-  function implementAddTodo(e){
+  const [todoList, setTodoList] = useState([]);
+
+  console.log("todo", todo)
+  function implementAddTodo(e) {
     e.preventDefault();
     setTodoList([...todoList, todo])
     setTodo("")
   }
 
-  function handleDelete(index){
-    setTodoList(todoList.filter((value,position) => position != index))
+  function handleDelete(index) {
+    setTodoList(todoList.filter((value, position) => position != index))
   }
 
 
   return (
     <div>
       <form onSubmit={implementAddTodo}>
-        <input type="text" placeholder="Enter task" onChange={(e) => setTodo (e.target.value)} value={todo}/>
+        <input type="text" placeholder="Enter task" onChange={(e) => setTodo(e.target.value)} value={todo} />
         <button type="submit">Add Todo</button>
       </form>
 
       {
-        todoList.map((item ,index)=> (
+        todoList.map((item, index) => (
           <ul>
-            <div>
-              {item}
+            <div key={index}>
+              <li>
+                {item}
+              </li>
               <button onClick={() => handleDelete(index)}>Delete</button>
             </div>
           </ul>
